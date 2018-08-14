0cd251f0cd84a61d37a769102f90cbf4a6d09c7b
wanghaobing <wanghaobing@wanghaobing.local>
2018-08-14 15:41:23 +0800

    Chapter 2.4 CollectionTypes

---
+++ Swift4.2/Swift4.2/Chapter02/CollectionTypes.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionTypes.swift
+//  Swift4.2
+//
+//  Created by 王浩兵 on 2018/8/14.
+//  Copyright © 2018年 爨乡的云. All rights reserved.
+//
+
+import UIKit
+
+class CollectionTypes: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "集合类型"
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
